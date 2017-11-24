6ccc15c29e48dcdb89d2239cd7ca8242816b5305
Adrian Sitko <adrian.sitko@nokia.com>
2017-11-24 16:22:23 +0100

    to_s in Entry and Record

--- entry.rb
+++ entry.rb
@@ -6,6 +6,10 @@ class Entry
       @station = station
       @time = time
     end
+
+    def to_s
+      "#{@station} @ #{@time}"
+    end
   end
 
   attr_reader :to, :from, :time
@@ -14,5 +18,9 @@ class Entry
     @from = Record.new start, depart
     @to = Record.new stop, arriv
     @time = time
+  end
+
+  def to_s
+    "#{@from} -> #{@to} (in #{@time}) "
   end
 end
