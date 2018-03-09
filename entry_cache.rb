775e510eb745121375b335cb4e4e904ae9574d43
Adrian Sitko <adrian.sitko@nokia.com>
2018-03-09 08:34:40 +0100

    Added logs to EntryCache

--- entry_cache.rb
+++ entry_cache.rb
@@ -1,4 +1,5 @@
 require_relative 'entry_mapper'
+require_relative 'log'
 
 class EntryCache
   def initialize(query)
@@ -8,17 +9,22 @@ class EntryCache
 
   def get_conn_after(time)
     closest_entry = find_entry_closest_to(time)
-    return closest_entry if closest_entry
+    if closest_entry
+      log "Taking closest entry from cache: #{closest_entry}"
+      return closest_entry
+    end
     fill_cache(time)
     find_entry_closest_to(time)
   end
 
+  private
   def fill_cache(time)
+    log 'Filling cache with query for time ' + time.to_s
     @entries += EntryMapper.new(@query.at(time).get).map_rows
   end
 
-  private
   def find_entry_closest_to(time)
+    log 'Searching for closest entry in cache'
     @entries.select {|e| e.source.time > time}.min_by {|e| e.source.time}
   end
 end
