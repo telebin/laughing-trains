7abe705c588ded3ca5d2cabcde1d3040e94f0f1d
Adrian Sitko <adrian.sitko@nokia.com>
2018-10-17 13:11:03 +0200

    Changed hardcoded route in daemon

--- daemon.rb
+++ daemon.rb
@@ -5,23 +5,28 @@ require_relative 'entry_cache'
 require_relative 'notification'
 require_relative 'gir_notification_executor'
 
+# TODO make configurable
+MINIMAL_DELAY = 30.minutes
+
 class Daemon
   def initialize
     @notifier = GirNotificationExecutor.new
-    @mik_wro_cache = EntryCache.new PkpQuery.new.from(LOCATIONS[:mik]).to(LOCATIONS[:wro])
-    @wro_zach_cache = EntryCache.new PkpQuery.new.from(LOCATIONS[:wro]).to(LOCATIONS[:zach])
+# TODO make route configurable
+    @mik_cze_cache = EntryCache.new PkpQuery.new.from(LOCATIONS[:mik]).to(LOCATIONS[:cze])
+    #@wro_zach_cache = EntryCache.new PkpQuery.new.from(LOCATIONS[:wro]).to(LOCATIONS[:zach])
   end
 
   def run
     while true
       # TODO add support for closer trains (i.e. 10 min from now - instant notification?)
       now = Time.now
-      soonest_conn = @mik_wro_cache.get_conn_after(now + 30.minutes)
-      change_conn = @wro_zach_cache.get_conn_after(soonest_conn.destination.time + 6.minutes)
+      delayed_time = now + MINIMAL_DELAY
+      soonest_conn = @mik_cze_cache.get_conn_after(delayed_time)
+      #change_conn = @wro_zach_cache.get_conn_after(soonest_conn.destination.time + 6.minutes)
 
-      log "Now waiting #{soonest_conn.source.time - (now + 30.minutes)}s with notification..."
-      sleep(soonest_conn.source.time - (now + 30.minutes))
-      notification = Notification.new 'Interesting trainz', soonest_conn, change_conn
+      log "Now waiting #{soonest_conn.source.time - delayed_time}s with notification..."
+      sleep(soonest_conn.source.time - delayed_time)
+      notification = Notification.new 'Interesting trainz', soonest_conn
       log 'Executing notification'
       @notifier.execute notification
     end
