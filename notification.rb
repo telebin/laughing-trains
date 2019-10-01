58e5cdb9c0e17796832da6bbcc0cf33e50a918c2
Adrian Sitko <adrian.sitko@nokia.com>
2019-10-01 13:57:22 +0200

    Updated notification body apparently

--- notification.rb
+++ notification.rb
@@ -10,7 +10,8 @@ class Notification
 
   def generate_body
     first = @connections.first
-    "<b>#{first.source.time.strftime '%H:%M'}</b> -(#{first.travel_time})> #{first.destination.name}#{@connections.count > 1 ? ', and then:' : ''}\n" +
+    "<b>#{first.source.time.strftime '%H:%M'}</b> -(#{first.travel_time})> #{first.destination.name}" +
+        "#{@connections.count > 1 ? ', and then:' : "(#{first.destination.time.strftime '%H:%M'})"}\n" +
         @connections[1..-1].map {|c| [c.source.time, c.travel_time, c.destination.name, c.destination.time]}.
             map {|a, b, c, d| "<b>#{a.strftime '%H:%M'}</b> -(#{b})> #{c} (#{d.strftime '%H:%M'})\n"}.join
   end
