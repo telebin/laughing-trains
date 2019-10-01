6e3fd4956da91a4cf215fbfc464988d67e01db35
Adrian Sitko <adrian.sitko@nokia.com>
2019-10-01 13:57:42 +0200

    Updated parameter parsing

--- train_parser.rb
+++ train_parser.rb
@@ -16,9 +16,9 @@ if ARGV.count.zero?
   Daemon.new.run
 else
   require 'time'
-  SOURCE_STATION_ID = LOCATIONS[ARGV[0].to_s.to_sym] || LOCATIONS[:zach]
+  SOURCE_STATION_ID = LOCATIONS[ARGV[0].to_s.to_sym] || LOCATIONS[:cze]
   DEST_STATION_ID = LOCATIONS[ARGV[1].to_s.to_sym] || LOCATIONS[:wro]
-  TIME = Time.parse ARGV[2]
+  TIME = ARGV[2] ? Time.parse(ARGV[2]) : Time.now + 30.minutes
 
   puts EntryMapper.new(PkpQuery.new.from(SOURCE_STATION_ID).to(DEST_STATION_ID).at(TIME).get).map_rows.map(&:to_s)
 end
